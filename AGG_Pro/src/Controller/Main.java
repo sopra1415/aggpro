1cafd5efd16eef80ea1c24375a9445ffd9271578
Heiko G. <>
2014-12-21 19:47:07 +0100

    Main.java erstellt LoginFrame erstellt AGG Pro Tournament System Logo in ProjektOrdner geladen

---
+++ AGG_Pro/src/Controller/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+/**
+ *
+ * @author Heiko Geppert
+ */
+public class Main {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        
+        System.out.println("start");
+        
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new View.Login.LoginFrame().setVisible(true);
+            }
+        });
+    }
+}
