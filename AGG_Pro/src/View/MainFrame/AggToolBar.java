52f85503b89eaa35cc1cd161b6d8e680e499a926
Tobi <tma.9550@gmail.com>
2014-12-30 12:20:27 +0100

    HelpFrame added and ActionListener implemented

--- AGG_Pro/src/View/MainFrame/AggToolBar.java
+++ AGG_Pro/src/View/MainFrame/AggToolBar.java
@@ -13,6 +13,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.*;
+import View.MainFrame.Help.HelpFrame;
 
 /**
  *
@@ -104,6 +105,14 @@ public class AggToolBar extends JToolBar{
                 main.dispose();
                 View.Login.LoginFrame f = new LoginFrame();
                 f.setVisible(true);
+            }
+        });
+        
+        btnHelp.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+               HelpFrame helpFrame = new HelpFrame();
             }
         });
     }
