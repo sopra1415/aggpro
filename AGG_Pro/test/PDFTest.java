98af58149d8771ad90ad4a4d79e853572be09855
Jonathan <jojogoe@gmx.de>
2015-01-12 00:12:54 +0100

    PDF Klasse

--- AGG_Pro/test/PDFTest.java
+++ AGG_Pro/test/PDFTest.java
@@ -11,6 +11,8 @@ import com.itextpdf.text.DocumentException;
 import Data.LiveClasses.*;
 import Controller.Exchange.PDF;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 
@@ -19,9 +21,15 @@ public class PDFTest {
 	@Test
 	public void test() throws DocumentException, IOException{
 		PDF pdf = new PDF();
-                pdf.appendParagraph("das ist ein test");
-		pdf.appendParagraph("Vorgegebene	Technologien	• • • • • Java 1.7 grafische Benutzeroberfläche mit Swing H2 mit JDBC iText Verwendung zusätzlicher externer Bibliotheken muss von den Betreuern vorher genehmigt werden (Antrag per Beitrag im Ilias-Forum) •  eigenständige Wahl der Werkzeuge");
-	pdf.create("/tmp/aggpro/test.pdf");
+                pdf.appendParagraph("AnfangsText");
+                ArrayList<ArrayList<String>> table = new ArrayList<>();
+                table.add(new ArrayList(Arrays.asList((new String[] {"Id","name","punkte"}))));
+                table.add(new ArrayList(Arrays.asList((new String[] {"1,","anna","10"}))));
+                table.add(new ArrayList(Arrays.asList((new String[] {"2","bernd","20"}))));
+                pdf.appendTable("Test123",table);
+		
+		pdf.appendParagraph("EndText");
+	pdf.write("/tmp/aggpro/test.pdf");
         }
         
 
